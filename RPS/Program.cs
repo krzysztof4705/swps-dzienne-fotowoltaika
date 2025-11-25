44eb69189caf6601aab6e53e6d88353466772b99
4705 <twojastara@gmail.com>
2025-11-25 11:11:25 +0100

    cleanup

--- RPS/Program.cs
+++ RPS/Program.cs
@@ -4,7 +4,7 @@ string GetCorrectSign(string playerName)
     Console.WriteLine($"{playerName}, choose your sign ({string.Join('/', allowedSigns)})");
     string sign = Console.ReadLine()!;
 
-    // while (!(firstSign.Equals(allowedSigns[0], stringComparison) || firstSign.Equals(allowedSigns[1], stringComparison) || firstSign.Equals(allowedSigns[2], stringComparison)))
+   
     while (!allowedSigns.Contains(sign, StringComparer.OrdinalIgnoreCase))
     {
         Console.WriteLine($"{playerName}, choose correct sign ({string.Join('/', allowedSigns)})");
@@ -18,17 +18,7 @@ const StringComparison stringComparison = StringComparison.OrdinalIgnoreCase;
 
 Console.WriteLine("Let's play Rock-Paper-Scissors!");
 
-// Console.WriteLine($"Player 1, choose your sign ({string.Join('/', allowedSigns)})");
-// Console.WriteLine($"Player 1, choose your sign ({string.Join("/", allowedSigns)})");
-// Console.WriteLine($"Player 1, choose your sign ({allowedSigns[0]}/{allowedSigns[1]}/{allowedSigns[2]})");
-// string firstSign = Console.ReadLine()!;
 
-// while (!(firstSign.Equals(allowedSigns[0], stringComparison) || firstSign.Equals(allowedSigns[1], stringComparison) || firstSign.Equals(allowedSigns[2], stringComparison)))
-// while (!allowedSigns.Contains(firstSign, StringComparer.OrdinalIgnoreCase))
-// {
-//     Console.WriteLine($"Player 1, choose correct sign ({string.Join('/', allowedSigns)})");
-//     firstSign = Console.ReadLine()!;
-// }
 string firstSign = GetCorrectSign("Player 1");
 string secondSign = GetCorrectSign("Player 2");
 
